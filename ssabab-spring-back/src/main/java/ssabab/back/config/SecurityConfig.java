db5b701c732ec544581455861c34aefcd4d69f20
김형석 <henna6967@gmail.com>
2025-06-25 18:20:12 +0900

    fix: 환경 변수 주입을 위한 Config,AccountController 변경

--- ssabab-spring-back/src/main/java/ssabab/back/config/SecurityConfig.java
+++ ssabab-spring-back/src/main/java/ssabab/back/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package ssabab.back.config;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.Cookie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -15,7 +14,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
@@ -25,12 +23,10 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfigurationSource;
-import ssabab.back.dto.TokenDTO;
 import ssabab.back.entity.Account;
 import ssabab.back.jwt.JwtAuthenticationFilter;
 import ssabab.back.jwt.JwtTokenProvider;
 import ssabab.back.repository.AccountRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Optional;
@@ -39,8 +35,6 @@ import java.nio.charset.StandardCharsets;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
