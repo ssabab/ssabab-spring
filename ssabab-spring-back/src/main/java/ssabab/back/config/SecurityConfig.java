29c467dda55e5f19d96d3ef570b99ef169a7805d
김형석 <henna6967@gmail.com>
2025-07-01 21:53:00 +0900

    cors origin check

--- ssabab-spring-back/src/main/java/ssabab/back/config/SecurityConfig.java
+++ ssabab-spring-back/src/main/java/ssabab/back/config/SecurityConfig.java
@@ -189,7 +189,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://ssabab.com"));
+//        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://ssabab.com"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
