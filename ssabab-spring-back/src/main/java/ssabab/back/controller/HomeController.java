6ca63a47edecd76459d2ddfb851ddc986db6ca0d
김형석 <henna6967@gmail.com>
2025-07-01 10:46:58 +0900

    fix: / redirect 경로 수정

--- ssabab-spring-back/src/main/java/ssabab/back/controller/HomeController.java
+++ ssabab-spring-back/src/main/java/ssabab/back/controller/HomeController.java
@@ -26,24 +26,8 @@ public class HomeController {
     private final AccountService accountService;
 
     @GetMapping("/")
-    public ResponseEntity<Object> home(Authentication authentication) {
-        if (authentication == null || !authentication.isAuthenticated()) {
-            return ResponseEntity.status(401).body(Map.of("error", "Unauthorized"));
-        }
-
-        String username = "Guest"; // 기본값
-        Object principal = authentication.getPrincipal();
-
-        // JWT 인증 후에는 UserDetails 타입으로 principal이 설정됩니다.
-        if (principal instanceof UserDetails userDetails) {
-            username = userDetails.getUsername(); // 보통 email이 username으로 사용됨
-        } else if (principal instanceof String) {
-            username = (String) principal; // 만약 principal이 String 형태의 username/email인 경우
-        }
-
-        Map<String, String> resp = new HashMap<>();
-        resp.put("message", "Welcome " + username + "!");
-        return ResponseEntity.ok(resp);
+    public ResponseEntity<Object> home() {
+        return ResponseEntity.status(200).body(Map.of("success", "status : 200"));
     }
 
     @GetMapping("/analysis")
